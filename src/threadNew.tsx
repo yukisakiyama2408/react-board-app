e46ff8d284758cb2141be5902a44b833c0754bfd
Sakiyama Yuki <yuki.king.0422@gmail.com>
2022-07-08 20:23:52 +0900

    add
    スレッド作成コンポーネントの追加

---
+++ src/threadNew.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Box,
+  TextField,
+  Button,
+  Avatar,
+  CssBaseline,
+  Grid,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+import { Controller } from "react-hook-form";
+
+const theme = createTheme();
+
+const threadNew = () => {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+    criteriaMode: "all",
+    shouldFocusError: false,
+  });
+
+  const urlThreadApi =
+    "https://railway-react-bulletin-board.herokuapp.com/threads";
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    axios
+      .post(urlThreadApi, {
+        title: data.title,
+      })
+      .then(function (response) {
+        console.log(response);
+        navigate("/");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              {/* <LockOutlinedIcon /> */}
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              ユーザー登録
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Controller
+                    name="title"
+                    control={control}
+                    rules={{
+                      required: "入力必須です。",
+                      maxLength: {
+                        value: 30,
+                        message: "30文字以下で入力してくださいね！",
+                      },
+                    }}
+                    render={({
+                      field: { onBlur, onChange, value },
+                      fieldState: { error },
+                    }) => (
+                      <TextField
+                        label="スレッド名"
+                        required
+                        fullWidth
+                        id="fullName"
+                        value={value}
+                        variant="outlined"
+                        margin="normal"
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        error={Boolean(error)}
+                        helperText={error?.message}
+                      />
+                    )}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                スレッドを作成する
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export { threadNew };
