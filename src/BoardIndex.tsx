8598253c4e35e54bc3f233e9fe3abfe62384a3a2
Sakiyama Yuki <yuki.king.0422@gmail.com>
2022-07-08 21:44:48 +0900

    update
    スレッド一覧のUIを修正

--- src/BoardIndex.tsx
+++ src/BoardIndex.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, Container, Card, CardContent } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 const BoardIndex = () => {
@@ -24,16 +24,31 @@ const BoardIndex = () => {
   }, []);
   return (
     <>
-      {threads.map((data) => {
-        return (
-          <div key={data.id}>
-            <div>{data.title}</div>
-          </div>
-        );
-      })}
-      <Button component={Link} to={"/thread/new"}>
-        <AddIcon />
-      </Button>
+      <Container>
+        {threads.map((data) => {
+          return (
+            <div>
+              <div key={data.id} className="card-section">
+                <Card className="index-card">
+                  <CardContent>
+                    <div>{data.title}</div>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+          );
+        })}
+        <div className="add-btn">
+          <Button
+            component={Link}
+            to={"/thread/new"}
+            variant="contained"
+            endIcon={<AddIcon />}
+          >
+            スレッドを作成する
+          </Button>
+        </div>
+      </Container>
     </>
   );
 };
