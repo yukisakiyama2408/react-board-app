cadcbc09d0edc43f24cc9db7fd5cef0fe437b23e
Sakiyama Yuki <yuki.king.0422@gmail.com>
2022-07-09 19:20:37 +0900

    add
    スレッドないの投稿一覧用のコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThreadIndex } from "./Thread/ThreadIndex";
 import { ThreadNew } from "./Thread/ThreadNew";
+import { ThreadPostIndex } from "./Thread/Post/ThreadPostIndex";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<ThreadIndex />} />
           <Route path="/thread/new" element={<ThreadNew />} />
+          <Route path="/thread/:thread_id" element={<ThreadPostIndex />} />
         </Routes>
       </BrowserRouter>
     </>
